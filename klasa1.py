857a0e087839c8652a5851caa8d7298c2602e7c3
paulina_pyrc <paulina_pyrc@o2.pl>
2018-04-27 08:55:21 +0200

    Zmiany w trakcie cwiczen

---
+++ klasa1.py
@@ -0,0 +1,10 @@
+def potega3(x):
+	return x*x*x
+
+
+def main():
+    print( potega3(2) )
+
+main()
+
+
---
+++ klasa2.py
@@ -0,0 +1,19 @@
+def happyBirthday(nazwa):
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you, dear" + nazwa + ".")
+    print("Happy Birthday to you!")
+
+
+
+
+def main():
+    #pytanie o nazwe
+    nazwaUsera = input('Podaj imię:')
+
+
+main()
+
+
+
+
