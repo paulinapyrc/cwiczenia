d4db0a6e93584cee8cbabfbf782b53cab7a6450b
paulina_pyrc <paulina_pyrc@o2.pl>
2018-04-27 09:17:09 +0200

    Drugie zmiany na cwiczeniach

---
+++ alamakota.py
@@ -0,0 +1,9 @@
+def potega4(x):
+    return x*x*x*x
+#dwa do potegi czwartej
+
+def main():
+    print( potega4(2) )
+
+main()
+    
---
+++ prime.py
@@ -0,0 +1,22 @@
+#dolnaGranica = 10
+#gornaGranica = 100
+#dolnaGranica = int(input("Dolny zakres to: "))
+#gornaGranica = int(input("Gorny zakres to: "))
+
+def liczbyPierwsze(dolnaGranica, gornaGranica):
+    print("Liczby pierwsze od ",dolnaGranica," do",gornaGranica,"to:")
+    for liczba in range(dolnaGranica,gornaGranica + 1):
+       # prime numbers are greater than 1
+       if liczba > 1:
+           for i in range(2,liczba):
+               if (liczba % i) == 0:
+                   break
+           else:
+               print(liczba)
+
+def main():
+    dolnaGranica=int(input('Dolna granica:'))
+    gornaGranica=int(input('Gorna granica:'))
+    print(liczbyPierwsze(dolnaGranica,gornaGranica))
+
+main()
